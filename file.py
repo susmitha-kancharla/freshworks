7608e6d193c1dca5ae41ee0fc2f491fe0860a008
susmitha-kancharla <69210224+susmitha-kancharla@users.noreply.github.com>
2020-12-01 22:54:53 +0530

    Update file.py

--- file.py
+++ file.py
@@ -90,3 +90,9 @@ modify("susmitha",99)
 delete("susmitha")
 
 
+t1=Thread(target=(create or read or delete),args=(key_name,value,timeout))
+t1.start()
+t1.sleep()
+t2=Thread(target=(create or read or delete),args=(key_name,value,timeout))
+t2.start()
+t2.sleep()
